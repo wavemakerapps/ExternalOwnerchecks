a5c9d8352cd08c268a318b85ba9c20e1d9b3453d
apptest <appTest1@wavemaker.com>
2017-11-09 08:47:57 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "ExternalOwnerchecks",
+  "homePage" : "Main",
+  "name" : "ExternalOwnerchecks",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
